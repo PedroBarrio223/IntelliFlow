d1b11168481e84c1e151af238a1281efc039fc2f
pietroarantess <pietro.carmo.arantes06@gmail.com>
2026-09-21 15:59:27 -0300

    Mudança no info_usuarios.sql

--- info_usuarios.sql
+++ info_usuarios.sql
@@ -6,6 +6,12 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `info.usuarios` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `info.usuarios`;
 
+-- Criação da tabela chavesapi
+CREATE TABLE `chavesapi` (
+  `id` int(11) NOT NULL,
+  `chave` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- Criação da tabela clientes
 CREATE TABLE `clientes` (
   `id_cliente` int(11) NOT NULL,
@@ -33,6 +39,15 @@ CREATE TABLE `usuarios` (
   `cargo` varchar(75) DEFAULT 'leitor'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- Inserção dos dados genéricos para a tabela chavesapi (evita bloqueio no Git)
+INSERT INTO `chavesapi` (`id`, `chave`) VALUES
+(1, 'SUA_CHAVE_API_AQUI_1'),
+(2, 'SUA_CHAVE_API_AQUI_2');
+
+-- Inserção dos dados iniciais de documentos
+INSERT INTO `documentos` (`id`, `tipo`, `caminho`, `titular`) VALUES
+(1, 'CPF', 'documentos_processados\\CPF\\CPF_20260921_145117_07f6b044.png', '25895868755');
+
 -- Inserção dos dados iniciais de usuários
 INSERT INTO `usuarios` (`id_usuario`, `nome_usuario`, `email`, `senha`, `cargo`) VALUES
 (1, 'pietro', 'pietro@gmail.com', '123', 'leitor'),
@@ -40,6 +55,9 @@ INSERT INTO `usuarios` (`id_usuario`, `nome_usuario`, `email`, `senha`, `cargo`)
 (3, 'melisaSA', 'melissa@gmail.com', '123', 'administrador');
 
 -- Definição de chaves primárias
+ALTER TABLE `chavesapi`
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id_cliente`);
 
@@ -50,11 +68,14 @@ ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id_usuario`);
 
 -- Configuração de Auto Incremento
+ALTER TABLE `chavesapi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 ALTER TABLE `clientes`
   MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `documentos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 ALTER TABLE `usuarios`
   MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
